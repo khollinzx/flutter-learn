89e204f4a57a586d79f2b7d0e7cd194e0dfcf017
Khollinzx <collinsbenson0039@gmail.com>
2023-08-05 02:24:05 +0100

    quiz push

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:test_app/gradient_container/home_container.dart';
-
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: HomeContainer([
-          Color.fromARGB(194, 16, 66, 6),
-          Color.fromARGB(194, 192, 78, 8)
-        ]),
-      ),
-    ),
-  );
-}
 
 
